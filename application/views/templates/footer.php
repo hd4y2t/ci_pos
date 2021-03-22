d1338cf581a333faa13f8393b5cfae90b17ee18b
hd4y2t <hd4y2t@gmail.com>
2021-03-22 19:59:14 +0700

    pisah header footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+ <!-- Footer -->
+ <footer class="main-footer">
+     <div class="footer-left">
+         Copyright &copy; 2021 <small class="bullet ml-2 mr-2"></small><a href="#">Jac</a>
+     </div>
+     <div class="footer-right">
+         v1.0
+     </div>
+ </footer>
+ </div>
+ </div>
+
+ </body>
+
+ </html
